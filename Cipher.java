47996d9a15ce7beee14c2ad4442d66fbdc08ead1
Paul Choh <paulchoh@gmail.com>
2017-09-26 09:42:53 -0500

    Add files via upload

---
+++ Cipher.java
@@ -0,0 +1,62 @@
+/*
+ 
+ Paul Choh
+ CS 120: Computer Programming I 
+ Fall 2017
+ Cipher.java
+
+*/
+
+package assignment1;
+import java.util.Scanner; 
+
+public class Cipher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in); 
+		
+		System.out.println("Please enter five numbers between 0 and 19: ");
+		
+		int numberOne, numberTwo, numberThree, numberFour, numberFive;
+	 	
+		// get five digits
+		numberOne = scan.nextInt();
+		numberTwo = scan.nextInt();
+		numberThree = scan.nextInt();
+		numberFour = scan.nextInt();
+		numberFive = scan.nextInt(); 
+		
+		
+		// get sum of five numbers
+		System.out.println("\nYour sum is " + (numberOne + numberTwo + 
+						numberThree + numberFour + numberFive) + ".");
+		 
+		// store sum of five numbers in variable
+		int total; 
+		total = numberOne + numberTwo + numberThree + numberFour + numberFive; 
+		
+		// separate individual numbers
+		int onesPlace, tensPlace; 
+		onesPlace = total / 10; 
+		tensPlace = total % 10; 
+		
+		// get key
+		System.out.println("\nEnter a number between 0 to 9 for encoding: ");
+		
+		// store key to a variable
+		int key; 
+		key = scan.nextInt(); 
+		
+		// set formula for encryption
+		int formulaOnesPlace, formulaTensPlace; 
+		formulaOnesPlace = (onesPlace + key) % 10;
+		formulaTensPlace = (tensPlace + key) % 10; 
+		
+		// display the encoded numbers
+		System.out.println("\nYour encoded numbers are: " + formulaOnesPlace + formulaTensPlace);
+	}
+	
+
+}
